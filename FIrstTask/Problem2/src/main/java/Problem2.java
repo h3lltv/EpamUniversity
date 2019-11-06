56e49a5dcb7acc9c4291a41b2e71125e8fc6394d
Roman Zabudskiy <roma0432@gmail.com>
2019-11-06 18:31:24 +0200

    Palindrome task complete

--- FIrstTask/Problem2/src/main/java/Problem2.java
+++ FIrstTask/Problem2/src/main/java/Problem2.java
@@ -8,19 +8,19 @@ public class Problem2 {
     public static void main(String[] args) {
         int a = Integer.MAX_VALUE;
         long timeStart = System.currentTimeMillis();
+        generateNegitivePalindrome();
         generatePalindrome();
         System.out.println(map.size());
         System.out.println(System.currentTimeMillis() - timeStart);
-        writePalindrome(map);
+        //writePalindrome(map);
+        System.out.println(System.currentTimeMillis() - timeStart);
         //System.out.println(map);
-
     }
 
     private static long createPalindrome(long input, int base, int isOdd) {
         long n = input;
         long palindrome = input;
 
-        if (n > 0) {
             if (isOdd == 1) {
                 n /= base;
             }
@@ -29,33 +29,67 @@ public class Problem2 {
                 palindrome = palindrome * base + (n % base);
                 n /= base;
             }
+        return palindrome;
+    }
+
+    public static int showInt(String bitNegativeNumber) {
+        char[] charArr = bitNegativeNumber.toCharArray();
+        //int count = 0;
+
+        int k;
+        for (k = 0; k < 32; ++k) {
+            if (charArr[k] == '1') {
+                charArr[k] = '0';
+            } else {
+                charArr[k] = '1';
+            }
         }
 
-//        if (n < 0) {
-//
-//            int j = (int)input;
-//            int pal = j;
-//            while (j < 0) {
-////                palindrome = ~palindrome;
-//             //   pal = pal >> 16;
-//              //  pal = Integer.rotateLeft(pal, 16);
-//
-//               // pal = pal >>> 8;
-//                palindrome = (Integer.reverse(pal) * base + (j % base));
-//                //n= n>>>8;
-//                //n=~n;
-//                j /= base;
-//              //  pal = Integer.rotateLeft(pal, 16);
-//            }
-//            System.out.println(palindrome);
-//        }
-        return palindrome;
+        for (k = 31; charArr[k] == '1'; --k) {
+            charArr[k] = '0';
+        }
+
+        charArr[k] = '1';
+        String string = new String(charArr);
+        //Integer.parseInt(string, 2);
+        return -Integer.parseInt(string, 2);
+    }
+
+    private static void generateNegitivePalindrome() {
+        char[] negativePolindromes = new char[16];
+        String bitNegativeNumber;
+        String halfBit;
+        int result;
+        negativePolindromes[0] = '1';
+
+        for (int j = 1; j < 16; ++j) {
+            negativePolindromes[j] = '0';
+        }
+        int k = 15;
+
+        for (int i = 32767; i > 0; --i) {
+            if (negativePolindromes[k] != '1') {
+                negativePolindromes[k] = '1';
+            } else {
+                for (k = 15; negativePolindromes[k] == '1'; --k) {
+                    negativePolindromes[k] = '0';
+                }
+
+                negativePolindromes[k] = '1';
+            }
+
+            halfBit = new String(negativePolindromes);
+            bitNegativeNumber = halfBit + (new StringBuffer(halfBit)).reverse().toString();
+            result = showInt(bitNegativeNumber);
+            map.put(result, Integer.toBinaryString(result));
+        }
+        //System.out.println(bitNegativeNumber);
     }
 
     private static void generatePalindrome() {
         long number;
         for (int j = 0; j < 2; j++) {
-            int i = 1;
+            int i = 0;
             while ((number = createPalindrome(i, 2, j % 2)) < Integer.MAX_VALUE) {
                 i++;
                 map.put((int) number, Integer.toBinaryString((int) number));
