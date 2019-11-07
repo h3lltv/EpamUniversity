9ecc0677f0a72756fa5840833057b0d819cc4695
Roman Zabudskiy <roma0432@gmail.com>
2019-11-07 23:32:12 +0200

    Solution simplified

--- FIrstTask/Problem2/src/main/java/Problem2.java
+++ FIrstTask/Problem2/src/main/java/Problem2.java
@@ -6,84 +6,57 @@ public class Problem2 {
     private static HashMap<Integer, String> map = new HashMap<>();
 
     public static void main(String[] args) {
-        int a = Integer.MAX_VALUE;
         long timeStart = System.currentTimeMillis();
-        generateNegitivePalindrome();
+        generateNegativePalindrome();
         generatePalindrome();
         System.out.println(map.size());
-        System.out.println(System.currentTimeMillis() - timeStart);
-        //writePalindrome(map);
-        System.out.println(System.currentTimeMillis() - timeStart);
-        //System.out.println(map);
+        System.out.println("map filled in " + (System.currentTimeMillis() - timeStart) + " msec");
+        writePalindrome(map);
+        System.out.println("written in " + (System.currentTimeMillis() - timeStart) / 1000 + " sec");
+        // System.out.println(map);
     }
 
     private static long createPalindrome(long input, int base, int isOdd) {
         long n = input;
         long palindrome = input;
 
-            if (isOdd == 1) {
-                n /= base;
-            }
+        if (isOdd == 1) {
+            n /= base;
+        }
 
-            while (n > 0) {
-                palindrome = palindrome * base + (n % base);
-                n /= base;
-            }
+        while (n > 0) {
+            palindrome = palindrome * base + (n % base);
+            n /= base;
+        }
         return palindrome;
     }
 
-    public static int showInt(String bitNegativeNumber) {
-        char[] charArr = bitNegativeNumber.toCharArray();
-        //int count = 0;
-
-        int k;
-        for (k = 0; k < 32; ++k) {
-            if (charArr[k] == '1') {
-                charArr[k] = '0';
-            } else {
-                charArr[k] = '1';
-            }
-        }
-
-        for (k = 31; charArr[k] == '1'; --k) {
-            charArr[k] = '0';
-        }
-
-        charArr[k] = '1';
-        String string = new String(charArr);
-        //Integer.parseInt(string, 2);
-        return -Integer.parseInt(string, 2);
-    }
-
-    private static void generateNegitivePalindrome() {
-        char[] negativePolindromes = new char[16];
+    private static void generateNegativePalindrome() {
+        boolean stop = false;
+        char[] negativePalindrome = new char[16];
         String bitNegativeNumber;
         String halfBit;
-        int result;
-        negativePolindromes[0] = '1';
+        negativePalindrome[0] = '1';
 
         for (int j = 1; j < 16; ++j) {
-            negativePolindromes[j] = '0';
+            negativePalindrome[j] = '0';
         }
         int k = 15;
-
-        for (int i = 32767; i > 0; --i) {
-            if (negativePolindromes[k] != '1') {
-                negativePolindromes[k] = '1';
+        while (!stop) {
+            if (negativePalindrome[k] != '1') {
+                negativePalindrome[k] = '1';
             } else {
-                for (k = 15; negativePolindromes[k] == '1'; --k) {
-                    negativePolindromes[k] = '0';
+                for (k = 15; negativePalindrome[k] == '1'; --k) {
+                    negativePalindrome[k] = '0';
                 }
-
-                negativePolindromes[k] = '1';
+                negativePalindrome[k] = '1';
             }
-
-            halfBit = new String(negativePolindromes);
+            halfBit = new String(negativePalindrome);
             bitNegativeNumber = halfBit + (new StringBuffer(halfBit)).reverse().toString();
-            result = showInt(bitNegativeNumber);
-            map.put(result, Integer.toBinaryString(result));
+            long a = Long.parseLong(bitNegativeNumber, 2);
+            map.put((int) a, Integer.toBinaryString((int) a));
+            if ((int) a == -1) stop = true;
         }
-        //System.out.println(bitNegativeNumber);
     }
 
     private static void generatePalindrome() {
@@ -96,7 +69,6 @@ public class Problem2 {
             }
         }
     }
-
 
     private static void writePalindrome(HashMap<Integer, String> hm) {
         hm.entrySet().forEach(entry -> {
--- FIrstTask/Problem8/src/main/java/Shop.java
+++ FIrstTask/Problem8/src/main/java/Shop.java
@@ -1,3 +1,64 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Scanner;
+//
+//public class Shop {
+//    static ArrayList<Flower> flowers = new ArrayList<>();
+//
+//    private void addFlower(Flower flower) {
+//        flowers.add(flower);
+//    }
+//
+//    private Bouquet addToBouquet(int maxValue) {
+//        Bouquet bouquet = new Bouquet();
+//        while (bouquet.getCost() < maxValue) {
+//            for (Flower f : flowers) {
+//                if ((bouquet.getCost() + f.getPrice()) <= maxValue && f.getNumber() > 0) {
+//                    bouquet.addFlower(f);
+//                    f.decrementNumber();
+//                }
+//            }
+//        }
+//        int size = bouquet.getSize();
+//        if (size % 2 != 1) {
+//            bouquet.deleteFlower();
+//        }
+////        System.out.println(bouquet.getCost());
+////        System.out.println("size " + bouquet.getSize());
+//        return bouquet;
+//    }
+//
+//    private static void createBouquet(){
+//        Scanner sc = new Scanner(System.in);
+//        Bouquet bq = new Bouquet();
+//
+//        Shop shop = new Shop();
+//        Flower flower1 = new Flower("Flower1", 1, 10);
+//        Flower flower2 = new Flower("Flower2", 2, 10);
+//        Flower flower3 = new Flower("Flower3", 3, 10);
+//        Flower flower4 = new Flower("Flower4", 4, 0);
+//        Flower flower5 = new Flower("Flower5", 5, 10);
+//        Flower flower6 = new Flower("Flower6", 6, 10);
+//        Flower flower7 = new Flower("Flower7", 7, 1);
+//        shop.addFlower(flower1);
+//        shop.addFlower(flower2);
+//        shop.addFlower(flower3);
+//        shop.addFlower(flower4);
+//        shop.addFlower(flower5);
+//        shop.addFlower(flower6);
+//        shop.addFlower(flower7);
+//        System.out.println("Enter bouquet price:");
+//        bq = shop.addToBouquet(sc.nextInt());
+//        Collections.shuffle(flowers);
+//        Collections.sort(flowers);
+//        System.out.println(bq + "\n " + "Bouquet cost: " + bq.getCost());
+//    }
+//
+//    public static void main(String[] args) {
+//        createBouquet();
+//    }
+//}
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
